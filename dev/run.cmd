96913eceae44b9958891084db31ce8f10f35ae75
Dustin H <djherbis@gmail.com>
2019-01-12 17:39:04 -0500

    updating dev server config

--- dev/run.cmd
+++ dev/run.cmd
@@ -1,6 +1,7 @@
 setlocal
 set PORT=1313
-set HOST=http://%DEV_SERVER%
+set EXTERNAL_PORT=1314
+set HOST=https://%DEV_DJHERBIS_SERVER%:%EXTERNAL_PORT%
 set SITE_DIR=%DEV_GOPATH%/src/github.com/djherbis/djherbis.github.io/djherbis.com
 docker-compose --project-directory .. -f docker-compose.yml build
 docker-compose --project-directory .. -f docker-compose.yml up -d --remove-orphans
